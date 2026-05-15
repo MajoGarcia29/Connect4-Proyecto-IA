f1d044306b8f6264e0e372bb47c69d1db45005f5
MajoGarcia29 <majo290701@gmail.com>
2026-05-15 16:35:02 -0500

    reorganizacion de funciones y correccion de errores en el codigo

--- Majo-Branch/qlearning.py
+++ Majo-Branch/qlearning.py
@@ -4,14 +4,10 @@ from game_utils import key
 class TablaQ:
 
     def __init__(self):
-
         self._q = {}
         self._n = {}
 
-    def get(self,
-        board,
-        accion
-    ) -> float:
+    def get(self, board, accion) -> float:
 
         return self._q.get(
             key(board),
@@ -21,31 +17,19 @@ class TablaQ:
             0.0
         )
 
-    def update(
-        self,
-        board,
-        accion,
-        U: float
-    ) -> None:
+    def update(self, board, accion, U: float) -> None:
 
         k = key(board)
 
         if k not in self._q:
-
             self._q[k] = {}
-
             self._n[k] = {}
 
         if accion not in self._q[k]:
-
             self._q[k][accion] = 0.0
-
             self._n[k][accion] = 0
-
         self._n[k][accion] += 1
-
         n = self._n[k][accion]
-
         self._q[k][accion] += (
             U - self._q[k][accion]
         ) / n
